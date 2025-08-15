d5322c4b4e1a3f1a9bc23e1491785b1e2f75a233
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-08-15 12:39:59 +0300

    entry file with routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import SpotifyAuth from './components/SpotifyAuth';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/auth" element={<SpotifyAuth />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
